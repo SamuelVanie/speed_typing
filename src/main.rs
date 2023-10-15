27bb08c55b08cb1b0da424799cbc05d6614a90cf
SamuelVanie <samuelmichaelvanie@gmail.com>
2023-10-16 00:57:16 +0200

    feat: add text to the screen
    - Add the placeholder for the text
    - Add assets for the cars

--- src/main.rs
+++ src/main.rs
@@ -15,31 +15,70 @@ fn main() {
 		..default()
 	    }))
         .add_systems(Startup, spawn_camera)
-        .add_systems(Startup, spawn_test_sprite)
+        .add_systems(Startup, current_text)
         .run();
 }
 
 #[derive(Component)]
 pub struct Gojo;
 
-pub fn spawn_test_sprite(
-    mut commands: Commands,
-    window_query: Query<&Window, With<PrimaryWindow>>,
-    asset_server: Res<AssetServer>
-) {
-    let window = window_query.get_single().unwrap();
+// pub fn spawn_test_sprite(
+//     mut commands: Commands,
+//     window_query: Query<&Window, With<PrimaryWindow>>,
+//     asset_server: Res<AssetServer>
+// ) {
+//     let window = window_query.get_single().unwrap();
 
+//     commands.spawn(
+// 	(
+// 	    SpriteBundle {
+// 		transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+// 		texture: asset_server.load("tile.png"),
+// 		..default()
+// 	    },
+// 	    Gojo {},
+// 	));
+
+// }
+
+
+fn current_text (mut commands: Commands) {
     commands.spawn(
-	(
-	    SpriteBundle {
-		transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
-		texture: asset_server.load("tile.png"),
-		..default()
+	NodeBundle {
+	    style: Style {
+		width: Val::Percent(100.0),
+		height: Val::Percent(100.0),
+		justify_content: JustifyContent::Center,
+		padding: UiRect { top: Val::Px(50.0), ..Default::default() },
+		..Default::default()
 	    },
-	    Gojo {},
-	));
-
+	    ..Default::default()
+	})
+	.with_children(
+	    |parent|
+	    {
+		parent.spawn(TextBundle {
+		    style: Style {
+			..Default::default()
+		    },
+		    text: Text {
+			sections: vec![TextSection {
+			    value: "Hello World".to_string(),
+			    style: TextStyle {
+				font_size: 20.0,
+				color: Color::WHITE,
+				..Default::default()
+			    },
+			}],
+			alignment: TextAlignment::Center,
+			..Default::default()
+		    },
+		    transform: Transform::from_xyz(0.0, 0.0, 0.0),
+		    ..Default::default()
+		});
+	    });
 }
+
 
 pub fn spawn_camera(
     mut commands: Commands,
